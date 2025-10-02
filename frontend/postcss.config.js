f0bc5d72411f7512d691f11fbf386ca7da325149
ManojVivekAnandaPrabhakar <manojvivekanandaprabhakar@gmail.com>
2025-10-02 17:31:15 +0530

    deploy try 7

---
+++ frontend/postcss.config.js
@@ -0,0 +1,8 @@
+/** @type {import('postcss-load-config').Config} */
+export default {
+  // This tells Vite/PostCSS to run these two packages (plugins)
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
