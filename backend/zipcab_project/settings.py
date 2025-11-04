18179d599cc8a176b399c5c0dc923f4c8f938057
Manoj Vivekananda Prabhakar <manojvivekanandaprabhakar@gmail.com>
2025-11-04 13:23:20 +0530

    fix:updated vite configs upto react 19 update

--- backend/zipcab_project/settings.py
+++ backend/zipcab_project/settings.py
@@ -148,9 +148,9 @@ USE_TZ = True
 # STATIC & MEDIA
 # --------------------------------------
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend', 'dist', 'assets'),
+    BASE_DIR / '../frontend/dist',
 ]
 
 
