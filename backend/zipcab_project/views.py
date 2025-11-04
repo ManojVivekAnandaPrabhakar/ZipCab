fa8996287ddca46092732438f8b8bf46751f42a8
Manoj Vivekananda Prabhakar <manojvivekanandaprabhakar@gmail.com>
2025-11-04 14:34:18 +0530

    fix:staticfiles route

--- backend/zipcab_project/views.py
+++ backend/zipcab_project/views.py
@@ -1,12 +1,13 @@
 import os
-from django.http import FileResponse, HttpResponse
+from django.http import FileResponse, Http404
 from django.views import View
-from django.conf import settings
-
 
 class FrontendAppView(View):
     def get(self, request, *args, **kwargs):
-        index_path = os.path.join(settings.STATIC_ROOT, "index.html")
+        index_path = os.path.join(
+            os.path.dirname(__file__), 
+            "../static/frontend_build/index.html"
+        )
         if os.path.exists(index_path):
             return FileResponse(open(index_path, "rb"))
-        return HttpResponse("index.html not found", content_type="text/plain")
+        raise Http404("index.html not found")
